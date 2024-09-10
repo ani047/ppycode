be6a9462eea7bde7caa71e4eadfa979455afb497
ani047 <anilmeran80@gmail.com>
2024-09-10 16:05:21 +0530

    bubble sort

---
+++ sorting/bubble.py
@@ -0,0 +1,14 @@
+def bubble_sort(n):
+    l= len(n)
+    for i in range(l-1,0,-1):
+        
+        for j in range(i):
+            if n[j]>n[j+1]:
+                n[j],n[j+1]=n[j+1],n[j]
+                
+nums = [6,3,8,1,3,9,4,7,3,6,2,6,6]
+print("This is the Array Before Sorting...")
+print(nums)
+bubble_sort(nums)
+print("This is the Array After Sorting...")
+print(nums)
